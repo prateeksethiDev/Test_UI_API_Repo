461c0cdace5bc21d7ccdb60e29dd219b4f012657
Prateek <prateeksethiDev@gmail.com>
2021-01-09 15:13:33 +0530

    Replace standalone setup to docker based Kubernetes cluster managed selenium grid.

--- TestAutomation_UI_API/src/main/java/testBase/BrowserFactory.java
+++ TestAutomation_UI_API/src/main/java/testBase/BrowserFactory.java
@@ -1,5 +1,8 @@
 package testBase;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -7,6 +10,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -21,13 +25,19 @@ public class BrowserFactory {
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--incognito");
 			options.setAcceptInsecureCerts(true);
-			driver = new ChromeDriver(options);
+			//driver = new ChromeDriver(options);
+			try {
+				driver=new RemoteWebDriver(new URL("http://192.168.64.3:30001/wd/hub"),options);
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
 		}else if(browser.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			FirefoxOptions foptions=new FirefoxOptions();
 			foptions.addArguments("-private");
 			foptions.setAcceptInsecureCerts(true);
 			driver = new FirefoxDriver(foptions);
+			
 		}else if(browser.equalsIgnoreCase("ie")) {
 			WebDriverManager.iedriver().setup();
 			InternetExplorerOptions ioptions= new InternetExplorerOptions();
